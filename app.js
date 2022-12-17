728937f47a79b99913096bffc1cb10072ebf9e7e
Saba <saba.sadegh67@gmail.com>
2022-12-17 19:35:50 +0330

    set interval function and clear it

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const numbers = [...document.querySelectorAll(".number")];
 const updateCount = (el) => {
-  const value = parseInt(el.data.value);
+  const value = parseInt(el.dataset.value);
   const increment = Math.ceil(value / 1000);
 
   let initialValue = 0;
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+    if (initialValue > value) {
+      el.textContent = `${value}+`;
+      clearInterval(increaseCount);
+      return;
+    }
+    el.textContent = `${initialValue}+`;
+  }, 1);
 };
 
-numbers.forEach((item) => updateCount(item));
+numbers.forEach((item) => {
+  updateCount(item);
+});
