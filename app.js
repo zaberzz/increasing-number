9f18e037c637f089739ab0535ed4ce147ef1db83
Saba <saba.sadegh67@gmail.com>
2022-12-17 19:21:56 +0330

    creating updateContent function

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const numbers = [...document.querySelectorAll(".number")];
+const updateCount = (el) => {
+  const value = parseInt(el.data.value);
+  const increment = Math.ceil(value / 1000);
+
+  let initialValue = 0;
+};
 
 numbers.forEach((item) => updateCount(item));
